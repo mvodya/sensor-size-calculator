8bbcccd7b379877c4cf1355cac41648954416185
Mark Vodyanitskiy <mvodya@icloud.com>
2024-02-06 11:42:19 +1000

    Select button prototype

--- src/components/parameter.tsx
+++ src/components/parameter.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 function SelectButton() {
+  const [open, setOpen] = useState(0);
+
+  function onClick() {
+    setOpen(open ? 0 : 1);
+  }
+
   return (
-    <div className="flex w-8 justify-center bg-slate-100">
-      <div>S</div>
+    <div className="flex w-8 justify-center bg-slate-200" onClick={onClick}>
+      <div className=" text-center py-1 px-4">{open ? "X" : "S"}</div>
     </div>
   )
 }
@@ -24,7 +30,7 @@ export function Parameter(props: ParameterProps<string>) {
       <div className="flex border-4 rounded-md">
         <input type="number" id={props.id} value={props.value} onChange={e => props.handler(e.target.value)} className="w-full bg-slate-100 text-center p-1 truncate hover:text-clip" />
         <div className="bg-slate-200 text-center py-1 px-4">{props.unit}</div>
-        {/* <SelectButton /> */}
+        <SelectButton />
       </div>
     </div>
   )
@@ -38,7 +44,7 @@ export function ParameterDual(props: ParameterProps<{ a: string, b: string }>) {
         <input type="number" id={props.id + "_a"} value={props.value.a} onChange={e => props.handler({ ...props.value, a: e.target.value })} className="w-full bg-slate-100 text-center p-1 truncate hover:text-clip" />
         <div className="bg-slate-200 text-center py-1 px-4">{props.delimiter ?? "/"}</div>
         <input type="number" id={props.id + "_b"} value={props.value.b} onChange={e => props.handler({ ...props.value, b: e.target.value })} className="w-full bg-slate-100 text-center p-1 truncate hover:text-clip" />
-        {/* <SelectButton /> */}
+        <SelectButton />
       </div>
     </div>
   )
