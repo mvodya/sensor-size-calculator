f3330d019e290e8c89cba5ae2da03071b831753a
Mark Vodyanitskiy <mvodya@icloud.com>
2024-02-07 16:29:18 +1000

    Added icons support

--- src/components/parameter.tsx
+++ src/components/parameter.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ReactElement, useState } from "react";
 
 function SelectButton() {
   const [open, setOpen] = useState(0);
@@ -17,6 +19,7 @@ function SelectButton() {
 type ParameterProps<T extends string | { a: string, b: string }> = {
   id: string;
   title: string;
+  icon?: IconDefinition;
   value: T;
   handler: (value: T) => void;
   delimiter?: string;
@@ -26,7 +29,14 @@ type ParameterProps<T extends string | { a: string, b: string }> = {
 export function Parameter(props: ParameterProps<string>) {
   return (
     <div className="">
-      <div className="pb-1 font-light">{props.title}:</div>
+      <div className="pb-1 font-light">
+        {props.icon !== undefined &&
+          <FontAwesomeIcon
+            icon={props.icon}
+            className="fas pr-2"
+          />
+        }
+        {props.title}:</div>
       <div className="flex border-4 rounded-md">
         <input type="number" id={props.id} value={props.value} onChange={e => props.handler(e.target.value)} className="w-full bg-slate-100 text-center p-1 truncate hover:text-clip" />
         <div className="bg-slate-200 text-center py-1 px-4">{props.unit}</div>
@@ -39,7 +49,15 @@ export function Parameter(props: ParameterProps<string>) {
 export function ParameterDual(props: ParameterProps<{ a: string, b: string }>) {
   return (
     <div className="">
-      <div className="pb-1 font-light">{props.title}:</div>
+      <div className="pb-1 font-light">
+        {props.icon !== undefined &&
+          <FontAwesomeIcon
+            icon={props.icon}
+            className="fas pr-2"
+          />
+        }
+        {props.title}:
+      </div>
       <div className="flex border-4 rounded-md">
         <input type="number" id={props.id + "_a"} value={props.value.a} onChange={e => props.handler({ ...props.value, a: e.target.value })} className="w-full bg-slate-100 text-center p-1 truncate hover:text-clip" />
         <div className="bg-slate-200 text-center py-1 px-4">{props.delimiter ?? "/"}</div>
